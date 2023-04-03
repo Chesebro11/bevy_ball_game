186de2e8197a873a9674bad0eeb6e49d2cf2c2f8
Jared Chesebro <chesebrojared@gmail.com>
2023-04-03 15:58:55 -0700

    system sets and system set notes

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -5,14 +5,41 @@ pub mod systems;
 
 use systems::*;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
+// Doing the same the but in an enum
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub enum PlayerSystemSet {
+//     Movement,
+//     Confinement
+// }
+// How this would look within the plugin:
+// .configure_set(PlayerSystemSet::Movement.before(PlayerSystemSet::Confinement))
+// .add_sytsten(System.in_set(PlayerSystemSet::Movement))
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+        app.configure_set(MovementSystemSet.before(ConfinementSystemSet))
+            .add_startup_system(spawn_player)
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
 }
+
+// .before allows us to guarantee the order in which some sytems operate
+            // .add_system(player_movement.before(confine_player_movement))
+            // .add_system(confine_player_movement)//.after(player_movement)
+            // .add_systems(
+            //     (
+            //         player_movement,
+            //         confine_player_movement
+            //     ).chain() // This is another way of using .before or .after but the nested systems allows for better readabillity.
+            //)
--- src/star/mod.rs
+++ src/star/mod.rs
@@ -1,11 +1,11 @@
 use bevy::prelude::*;
 
 pub mod components;
-pub mod resources;
 mod systems;
+pub mod resources;
 
-use resources::*;
 use systems::*;
+use resources::*;
 
 pub const NUMBER_OF_STARS: usize = 10;
 pub const STAR_SIZE: f32 = 30.0;
@@ -14,8 +14,8 @@ pub struct StarPlugin;
 
 impl Plugin for StarPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<StarSpawnTimer>();
-        app.add_startup_system(spawn_stars);
-        app.add_systems((tick_star_spawn_timer, spawn_stars_over_time));
+        app.init_resource::<StarSpawnTimer>()
+        .add_startup_system(spawn_stars)
+        .add_systems((tick_star_spawn_timer, spawn_stars_over_time));
     }
 }
