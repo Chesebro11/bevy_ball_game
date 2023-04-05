6e41565fe51f94227d41eb8fdb9a0458fb6c747d
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-04-05 11:48:19 -0700

    despawn main menu

--- src/mainmenu/systems/layout.rs
+++ src/mainmenu/systems/layout.rs
@@ -9,7 +9,14 @@ pub fn spawn_main_menu (
     let main_menu_entity = build_main_menu(&mut commands, &asset_server);
 }
 
-pub fn despawn_main_menu() {}
+pub fn despawn_main_menu(
+    mut commands: Commands,
+    main_menu_query: Query<Entity, With<MainMenu>>
+) {
+    if let Ok(main_menu_entity) = main_menu_query.get_single() {
+        commands.entity(main_menu_entity).despawn_recursive();
+    }
+}
 
 pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
     let main_menu_entity = commands
