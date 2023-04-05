443ea16f9a94f6161c878792fe2c24a684e8b211
Jared Chesebro <chesebrojared@gmail.com>
2023-04-04 21:11:19 -0700

    IDE freaking out, commiting partial menu outline

--- src/mainmenu/mod.rs
+++ src/mainmenu/mod.rs
@@ -1,10 +1,23 @@
+mod components;
+mod styles;
+mod systems;
+
+
 use bevy::prelude::*;
+
+use systems::layout::*;
+
+use crate::AppState;
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(main_menu);
+        app
+        .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+
+        .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
+
     }
 }
 
