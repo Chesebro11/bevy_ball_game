f69cd3468772c5dfcae43d0722399a3cdb5019a9
Jared Chesebro <chesebrojared@gmail.com>
2023-04-05 22:11:08 -0700

    LETS GOOOO (The enitre mod.rs file for the player plugin was an old version

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -3,7 +3,11 @@ use bevy::prelude::*;
 pub mod components;
 pub mod systems;
 
+use crate::AppState;
+
 use systems::*;
+
+use super::SimulationState;
 
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
 pub struct MovementSystemSet;
@@ -11,37 +15,29 @@ pub struct MovementSystemSet;
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
 pub struct ConfinementSystemSet;
 
-// Doing the same the but in an enum
-// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-// pub enum PlayerSystemSet {
-//     Movement,
-//     Confinement
-// }
-// How this would look within the plugin:
-
-
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.configure_set(MovementSystemSet.before(ConfinementSystemSet))
-            .add_startup_system(spawn_player)
-            .add_system(player_movement.in_set(MovementSystemSet))
-            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
-            .add_system(enemy_hit_player)
-            .add_system(player_hit_star);
+        app
+            // Configure System Sets
+            .configure_set(MovementSystemSet.before(ConfinementSystemSet))
+            // On enter State
+            .add_system(spawn_player.in_schedule(OnEnter(AppState::Game)))
+            .add_systems(
+                (
+                    player_movement.in_set(MovementSystemSet),
+                    confine_player_movement.in_set(ConfinementSystemSet),
+                )
+                    .in_set(OnUpdate(AppState::Game))
+                    .in_set(OnUpdate(SimulationState::Running)),
+            )
+            .add_systems(
+                (enemy_hit_player, player_hit_star)
+                    .in_set(OnUpdate(AppState::Game))
+                    .in_set(OnUpdate(SimulationState::Running)),
+            )
+            // Exit State
+            .add_system(despawn_player.in_schedule(OnExit(AppState::Game)));
     }
 }
-
-
-// .add_system(System::in_set(PlayerSystemSet::Movement))
-
-// .before allows us to guarantee the order in which some sytems operate
-// .add_system(player_movement.before(confine_player_movement))
-// .add_system(confine_player_movement)//.after(player_movement)
-// .add_systems(
-//     (
-//         player_movement,
-//         confine_player_movement
-//     ).chain() // This is another way of using .before or .after but the nested systems allows for better readabillity.
-//)
--- src/game/ui/pause_menu/mod.rs
+++ src/game/ui/pause_menu/mod.rs
@@ -7,16 +7,17 @@ use bevy::prelude::*;
 use systems::layout::*;
 // use systems::interactions::*;
 
-// use crate::AppState;
+use crate::AppState;
 use crate::game::SimulationState;
 
 pub struct PauseMenuPlugin;
 
 impl Plugin for PauseMenuPlugin {
     fn build(&self, app:&mut App) {
-        app.add_system(spawn_pause_menu.in_schedule(OnEnter(SimulationState::Paused)));
+        app.add_system(spawn_pause_menu.in_schedule(OnEnter(SimulationState::Paused)))
         // .add_systems((
             // app.add_system(spawn_pause_menu);
         // ))
+        .add_system(despawn_pause_menu.in_schedule(OnEnter(SimulationState::Running)));
     }
 }
